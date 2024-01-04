701595da6ec8a2443995ab8e90bde71e196badd5
MattiaPaiano91 <paianomattia1@gmail.com>
2024-01-04 17:18:51 +0100

    -Esercizio completato + bonus

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+
+
+createApp({
+  data() {
+    return {
+      message: "Ciao belli!",
+      img: "img/pexels-kürşat-18748879.jpg",
+    };
+  },
+}).mount("#app");
+
+
